92c656d748f9ffbd206ce6cadec9df79bab2a2b4
Finn <finn.s.james@gmail.com>
2019-07-10 14:36:26 -0400

    nav menu added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 // custom scripts
+
+$(document).ready( function () {
+  var menu_visible = false;
+  $("#menu-button").on("click", function () {
+    if (menu_visible) {
+      $("#overlay").removeClass("menu-visible");
+      $("#compass").text("");
+      $("#menu-button").addClass("wobble");
+      menu_visible = false;
+    } else {
+      $("#overlay").addClass("menu-visible");
+      $("#compass").text("");
+      $("#menu-button").removeClass("wobble");
+      menu_visible = true;
+    }
+    
+    console.log("hey");
+  });
+});
